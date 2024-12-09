429b80980a46c2d3eb4e6a027b419f36dda8036e
Cadence Boyce <107009236+cadenxa@users.noreply.github.com>
2024-12-09 13:58:41 -0500

    Comments and textbook screenshots to present code

--- Code Generator/expressionfromxml.py
+++ Code Generator/expressionfromxml.py
@@ -79,10 +79,15 @@ def expression_code_gen(root):
     expression = Expression(root)
     code_lines = []
 
+    # Store our first term in a variable since we refer to it a lot
     term = expression.objects[0]
+
+    # if exp is op exp
     if isinstance(term.objects[0], UnaryOp):
         code_lines.append(expression_code_gen(term.objects[1:]))
         code_lines.append(UNARY_OP_TO_VM[term.objects[0].text])
+
+    # if exp is f(exp, exp...)
     elif isinstance(term.objects[0], SubroutineCall):
         num_args = 0
         for child in term.objects[0].objects:
@@ -92,13 +97,19 @@ def expression_code_gen(root):
                         code_lines.append(expression_code_gen(j.objects))
                         num_args += 1
         code_lines.append("call " + term.objects[0].name + " " + num_args)
+
+    # if exp is exp'1 op exp'2
     elif isinstance(term, Term) and isinstance(term.objects[1], Op) and isinstance(term.objects[2], Term):
         code_lines.append(expression_code_gen(term.objects))
         code_lines.append(expression_code_gen(expression.objects[2].objects))
         code_lines.append(OP_TO_VM[expression.objects[1].objects[0].text])
+
+    # if exp is varName
     elif term.objects[0].tag in ['identifier', 'keyword']:
         # todo: look up term.objects[0].text in symbol table and put memory segment + index in push statement
         code_lines.append("push " + term.objects[0].text)
+
+    # if exp is integerConstant
     elif term.objects[0].tag == 'integerConstant':
         code_lines.append("push constant " + term.objects[0].text)
     return code_lines
